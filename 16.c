ae128db686e7241b2a668703e1f8bc2affff114c
Abhishek0 <Abhishek0@linux.com>
2025-09-29 21:30:37 +0530

    added question 16

---
+++ 16.c
@@ -0,0 +1,46 @@
+/*
+============================================================================
+Name : 16.c
+Author : Abhishek Mandal
+Description : Write a program to send and receive data from parent to
+child vice versa. Use two way communication
+Date: 29th September 2025
+============================================================================
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
+int main(){
+	int fd1[2], fd2[2];
+	char buff1[50], buff2[50];
+	pipe(fd1);
+	pipe(fd2);
+	if(!fork()){
+		close(fd1[0]);
+		close(fd2[1]);
+		write(fd1[1], "hello from parent process!\n", 19);
+		read(fd2[0], buff2, sizeof(buff2));
+		printf("messasge form child: %s\n", buff2);
+	} else {
+		close(fd1[1]);
+		close(fd2[0]);
+		write(fd2[1], "hello from child process!\n", 18);
+		read(fd1[0], buff1, sizeof(buff1));
+		printf("message from parent: %s\n", buff1);
+	}
+	wait(0);
+	return 0;
+}
+/*
+============================================================================
+output:
+ab@ab:~/handson2$ cc 16.c
+ab@ab:~/handson2$ ./a.out
+messasge form child: hello from child process!
+
+message from parent: hello from parent process!
+
+============================================================================
+*/
+
