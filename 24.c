19784d4ed904ea0565320155771e836915a8ca4b
Abhishek0 <Abhishek0@linux.com>
2025-09-30 15:41:18 +0530

    added 24.c

--- 24.c
+++ 24.c
@@ -16,7 +16,7 @@ Date: 30th September 2025
 #include<fcntl.h>
 
 int main(){
-	key_t msgKey = ftok(".", 123456);
+	key_t msgKey = ftok("23.c", 123456);
 	int msgid = msgget(msgKey, IPC_CREAT);
 	printf("the key is: %d\n", msgKey);
 	printf("the message queue id is: %d\n", msgid);
@@ -27,7 +27,7 @@ int main(){
 output:
 ab@ab:~/handson2$ cc 24.c
 ab@ab:~/handson2$ ./a.out
-the key is: 1074146250
-the message queue id is: 1
+the key is: 1074143704
+the message queue id is: 2
 ============================================================================
 */
